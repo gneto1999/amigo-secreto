5fda6419a479e9a9f6dcf9b48891ab05e8763c37
GilmarSantos <gneto1999@hotmail.com>
2024-12-20 01:02:37 -0300

    arquivo .env para conexão com o BD e porta da API

--- src/conection.ts
+++ src/conection.ts
@@ -1,8 +1,15 @@
-import { Sequelize } from "sequelize";
+import { Sequelize } from 'sequelize'
+import dotenv from 'dotenv'
 
-const conn = new Sequelize("amigosecreto", "postgres", "ffc1902", {
-    host: "localhost",
-    dialect: "postgres",
+dotenv.config()
+const database = process.env.DATABASE || ''
+const host = process.env.HOST || ''
+const user = process.env.USER || ''
+const password = process.env.PASSWORD || ''
+
+const conn = new Sequelize(database, user, password, {
+    host: host,
+    dialect: 'postgres',
     logging: console.log
 })
 
--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,10 @@ import './models/Group'
 import './models/Participant'
 import './models/Match'
 import routes from './routes/index'
+import dotenv from 'dotenv'
+
+dotenv.config()
+const port = process.env.API_PORT || '3000'
 
 const app = express()
 
@@ -17,6 +21,6 @@ app.use('/api', routes)
 
 conn.sync({ force: false }) // force: false para evitar recriação de tabelas a cada reinício
     .then(() => {
-        app.listen(3000, () => console.log('Api running on port 3000....'));
+        app.listen(port, () => console.log(`Api running on ${port}....`));
     })
     .catch(err => console.log('Error syncing database:', err));
